d3f4074c1eac9f297f3579512cedd7edd87d3c7a
terahje <terahjequinn@gmail.com>
2021-02-09 11:53:23 -0700

    user can upload image

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const fileRoutes = require('./routes/file-upload');
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload');
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -14,8 +14,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(require('./routes'));
-app.use('/api/', fileRoutes);
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes); // add this route for image upload
 
 // Start the API server
 app.listen(PORT, () =>
---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      Bucket: 'user-images-f908bf05-7b01-4cca-9d1d-54a9efe7485a',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
